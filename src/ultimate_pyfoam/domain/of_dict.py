cce48772ef06756a276024c2b48b0f765707c61f
ARostekMU <rostek@mail.muni.cz>
2022-11-10 16:28:52 +0000

    improved, but how to add nested dict in typehint?

--- src/ultimate_pyfoam/domain/of_dict.py
+++ src/ultimate_pyfoam/domain/of_dict.py
@@ -1,22 +1,26 @@
 from collections import UserDict
+from collections.abc import Mapping
 from dataclasses import dataclass
 from textwrap import indent
+from typing import Any  # TODO: how to type hint nested dict?
+
+from ultimate_pyfoam.domain.dimensions import Dimensions
 
 
 @dataclass(init=False)
-class OfDict(UserDict[str, "str | OfDict | float | int"]):
+class OfDict(UserDict[str, Mapping[str, Any] | str | float | int | Dimensions]):
     """OfDict."""
 
     def __str__(self) -> str:
         string = ""
 
         for key, value in self.data.items():
-            if isinstance(value, OfDict):
+            if isinstance(value, Mapping):
                 string += f"{key}\n"
                 string += "{\n"
-                string += indent(str(value), "    ")
+                string += indent(str(OfDict(value)), "    ")
                 string += "}\n"
             else:
-                string += f"{key} {value};\n"
+                string += f"{key} {str(value)};\n"
 
         return string
