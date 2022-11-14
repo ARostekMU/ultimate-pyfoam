223c8f6b3293dcd411fbd341e0ed64eeb9d78e29
Ladislav Bartos <ladmeb@gmail.com>
2022-11-14 16:36:45 +0000

    Basic Implementation of a Dumper

--- src/ultimate_pyfoam/service/dumper.py
+++ src/ultimate_pyfoam/service/dumper.py
@@ -1,6 +1,24 @@
 from dataclasses import dataclass
 
+from pathlib import Path
+from ultimate_pyfoam.domain.of_dict import OfDict
 
 @dataclass()
 class Dumper:
-    pass
+    path: Path
+    content: OfDict
+    overwrite: bool = False
+
+    def write(self):
+        if self.path.is_dir():
+            raise IsADirectoryError
+        
+        if not self.overwrite and self.path.exists():
+            raise FileExistsError
+        
+        self.path.parents[0].mkdir(parents = True, exist_ok = True)
+
+        with self.path.open("w") as output:
+            output.write(str(self.content))
+
+
