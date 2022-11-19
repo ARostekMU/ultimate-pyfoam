af5c42a92e58cead91291ed2a1284710c231ea77
Ladislav Bartos <ladmeb@gmail.com>
2022-11-19 17:34:09 +0000

    Added type annotations

--- src/ultimate_pyfoam/service/dumper.py
+++ src/ultimate_pyfoam/service/dumper.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-
 from pathlib import Path
+
 from ultimate_pyfoam.domain.of_dict import OfDict
+
 
 @dataclass()
 class Dumper:
@@ -9,16 +10,14 @@ class Dumper:
     content: OfDict
     overwrite: bool = False
 
-    def write(self):
+    def write(self) -> None:
         if self.path.is_dir():
             raise IsADirectoryError
-        
+
         if not self.overwrite and self.path.exists():
             raise FileExistsError
-        
-        self.path.parents[0].mkdir(parents = True, exist_ok = True)
+
+        self.path.parents[0].mkdir(parents=True, exist_ok=True)
 
         with self.path.open("w") as output:
             output.write(str(self.content))
-
-
