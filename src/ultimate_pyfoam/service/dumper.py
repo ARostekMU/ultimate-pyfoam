45ce4189fc23c62b91c57142b14e4ca65bf0e940
alfonz47 <514564@mail.muni.cz>
2022-11-21 15:48:05 +0000

    dumper class

--- src/ultimate_pyfoam/service/dumper.py
+++ src/ultimate_pyfoam/service/dumper.py
@@ -1,6 +1,30 @@
+from dataclasses import InitVar
 from dataclasses import dataclass
+from dataclasses import field
+from pathlib import Path
+from typing import Any
+from typing import Mapping
+
+from ultimate_pyfoam.domain.of_dict import OfDict
 
 
 @dataclass()
 class Dumper:
-    pass
+    content: InitVar[Mapping[str, Any]]
+    content_ofdict: OfDict = field(init=False)
+
+    def __post_init__(self, content: Mapping[str, Any]) -> None:
+        self.content_ofdict = OfDict(content)
+
+    def write(self, path: Path, overwrite: bool = False) -> None:
+        if path.is_dir():
+            raise IsADirectoryError()
+
+        if not overwrite and path.exists():
+            raise FileExistsError()
+
+        if not path.parent.exists():
+            path.parent.mkdir(parents=True)
+
+        with path.open("w") as f:
+            f.write(str(self.content_ofdict))
