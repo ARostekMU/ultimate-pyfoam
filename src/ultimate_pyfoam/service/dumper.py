cfa7d9ce513a8150675c11f90a3a5b0b718c53cd
marstMUNI <483894@mail.muni.cz>
2022-11-21 16:19:17 +0000

    better dumper

--- src/ultimate_pyfoam/service/dumper.py
+++ src/ultimate_pyfoam/service/dumper.py
@@ -1,16 +1,22 @@
-from dataclasses import dataclass
 from collections.abc import Mapping
-from ultimate_pyfoam.domain.of_dict import OfDict
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Any
+
+from ultimate_pyfoam.domain.of_dict import OfDict
+
 
 @dataclass()
 class Dumper:
-    content: Mapping
+    content: Mapping[str, Any]
 
-    def write(self, path, overwrite=False) -> None:
+    def write(self, path: Path, overwrite: bool = False) -> None:
         path.parent.mkdir(parents=True, exist_ok=True)
         if path.exists() and not path.is_dir() and not overwrite:
-            raise FileExistsError('This file already exists. If you wish to overwrite, set "overwrite = True".')
+            raise FileExistsError(
+                'This file already exists. \
+                If you wish to overwrite, set "overwrite = True".'
+            )
         else:
-            with path.open('w') as f:
+            with path.open("w") as f:
                 f.write(str(OfDict(self.content)))
