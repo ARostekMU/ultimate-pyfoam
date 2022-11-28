8c089f260dbc7133e97950383aeb440962f76a67
Ladislav Bartos <ladmeb@gmail.com>
2022-11-28 15:37:11 +0000

    Implemented case dumper

--- src/ultimate_pyfoam/service/case_dumper.py
+++ src/ultimate_pyfoam/service/case_dumper.py
@@ -1,6 +1,17 @@
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+from typing import Mapping
+
+from ultimate_pyfoam.domain.of_dict import OfDict
+from ultimate_pyfoam.service.dumper import Dumper
 
 
 @dataclass()
 class CaseDumper:
-    pass
+    case: Mapping[Path, Mapping[str, Any]]
+
+    def dump(self, dump_dir: Path) -> None:
+        for path, content in self.case.items():
+            dumper = Dumper(content=content)
+            dumper.write(dump_dir / path)
