ccc97b1a21a1254862f700784ce62b7e9b3dbaf4
ARostekMU <rostek@mail.muni.cz>
2022-11-28 10:20:09 +0000

    progress

--- src/ultimate_pyfoam/service/case_dumper.py
+++ src/ultimate_pyfoam/service/case_dumper.py
@@ -1,6 +1,15 @@
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+from typing import Mapping
+
+from ultimate_pyfoam.service.dumper import Dumper
 
 
 @dataclass()
 class CaseDumper:
-    pass
+    case: Mapping[Path, Mapping[str, Any]]
+
+    def dump(self, case_dir: Path) -> None:
+        for rel_file_path, file_content in self.case.items():
+            Dumper(content=file_content).write(case_dir / rel_file_path)
--- tests/service/test_case_dumper.py
+++ tests/service/test_case_dumper.py
@@ -1,5 +1,53 @@
+from pathlib import Path
+
+from ultimate_pyfoam.domain.dimensions import Dimensions
 from ultimate_pyfoam.service.case_dumper import CaseDumper
 
 
 def test_init() -> None:
-    CaseDumper()
+    CaseDumper(case={})
+
+
+dummy_case = {Path("dummyFile"): {"dummyKey": "dummyValue"}}
+
+
+def test_dump_dummy(tmp_path: Path) -> None:
+    cd = CaseDumper(case=dummy_case)
+    cd.dump(case_dir=tmp_path)
+
+    assert (tmp_path / "dummyFile").exists()
+
+
+def test_dumped_content_is_correct(tmp_path: Path) -> None:
+    cd = CaseDumper(case=dummy_case)
+    cd.dump(case_dir=tmp_path)
+
+    written_string = Path(tmp_path / "dummyFile").read_text().strip()
+    string = "dummyKey dummyValue;"
+    assert string == written_string
+
+
+multidir_case = {
+    Path("a"): {"x": 1},
+    Path("b"): {"y": 2},
+    Path("C/d"): {"z": 3},
+    Path("E/F/g"): {"w": 4},
+}
+
+
+def test_dump_mutliple_directories(tmp_path: Path) -> None:
+    cd = CaseDumper(case=multidir_case)
+    cd.dump(case_dir=tmp_path)
+
+    assert (tmp_path / "a").exists()
+    assert (tmp_path / "b").exists()
+    assert (tmp_path / "C" / "d").exists()
+    assert (tmp_path / "E" / "F" / "g").exists()
+
+
+realistic_case = {
+    Path("0/U"): {"dimensions": Dimensions(0, 1, -1, 0, 0, 0, 0)},
+    Path("constant/thermophysicalProperties"): {"thermoType": {"type": "hePsiThermo"}},
+    Path("constant/turbulenceProperties"): {"simulationType": "laminar"},
+    Path("system/fvSchemes"): {"fluxScheme": "Kurganov"},
+}
