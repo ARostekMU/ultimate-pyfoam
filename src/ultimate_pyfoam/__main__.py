487e4f17ff521e761da94ef8b95557d095aff73d
hermajan <hermajan@gmail.com>
2022-12-05 15:02:04 +0000

    Added CLI

--- src/ultimate_pyfoam/__main__.py
+++ src/ultimate_pyfoam/__main__.py
@@ -1,11 +1,28 @@
 """Command-line interface."""
 import click
+import importlib
+from pathlib import Path
+from ultimate_pyfoam.service.case_dumper import CaseDumper
 
 
-@click.command()
+@click.group()
 @click.version_option()
 def main() -> None:
     """Ultimate PyFoam!!!."""
+
+
+@main.command
+@click.argument('case_file', default="case.py")
+def dump():
+    click.echo("aaa")
+
+    module = importlib.util.spec_from_file_location('case_file')
+    case = module.__getattr__("case")
+
+    cd = CaseDumper(case)
+    #cd.dump(temp_dir)
+
+    return 0
 
 
 if __name__ == "__main__":
--- src/ultimate_pyfoam/service/case_dumper.py
+++ src/ultimate_pyfoam/service/case_dumper.py
@@ -1,6 +1,17 @@
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+from typing import Mapping
+
+from ultimate_pyfoam.domain.of_dict import OfDict
+from ultimate_pyfoam.service.dumper import Dumper
 
 
 @dataclass()
 class CaseDumper:
-    pass
+    case: Mapping[str, Any]
+
+    def dump(self, dump_dir: Path):
+        for key, value in self.case.items():
+            dumper = Dumper(value)
+            dumper.write(dump_dir / key, True)
